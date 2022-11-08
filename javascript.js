9dadc940ccfafe04a859e31838b10fcf417cbd83
miguelvalden <miguelvalden77@gmail.com>
2022-11-08 14:53:40 +0100

    clase 8

---
+++ javascript.js
@@ -0,0 +1,45 @@
+
+
+
+
+// filter
+
+    // 1. Haz una función que reciba como parámetro un array de números y devuelva un array con los números pares del array anterior.
+
+
+    // 2. Tomando la función anterior, ahora haz que reciba un segundo parámetro, que será un string con las palabras "par" o "impar". Si la palabra es "par", se debe devolver un array con los números pares. Si la palabras es "impar" se debe devolver un array con los números impares
+
+
+    // 3. Haz una función que reciba como parámetro un array de strings y devuelva otro array con los strings que tengan una longitud de al menos 6 caracteres.
+
+
+    // 4. Dado un array de objetos, haz una función que devuelva otro array con las personas que tengan 18 años de edad o más.
+
+    let personas = [
+        {
+            nombre: "Juanpe",
+            edad: 19
+        },
+        {
+            nombre: "Pablo",
+            edad: 10
+        },
+        {
+            nombre: "Julieta",
+            edad: 5
+        },
+        {
+            nombre: "Estrella",
+            edad: 21
+        },
+        {
+            nombre: "Miguel Ángel pequeño",
+            edad: 15
+        },
+        {
+            nombre: "Lucía",
+            edad: 18
+        }
+    ]
+
+
